75aa66d381629b08942824d419ac5c3e88631030
Jake Dawkins <dawkinsjh@gmail.com>
2018-02-16 11:06:31 -0500

    add spread

---
+++ team-teaching/spread.test.js
@@ -0,0 +1,25 @@
+// Objects
+
+it('basic object spread (object clone)', () => {});
+
+it('basic object spread (merging objects)', () => {});
+
+// Arrays
+
+it('basic array spread (array clone)', () => {});
+
+it('basic array spread (concat arrays)', () => {});
+
+it('function params', () => {
+  const sum3 = (a, b, c) => a + b + c;
+});
+
+// rest syntax
+
+it('rest on function params', () => {
+  //sum3
+});
+
+it('rest on arrays', () => {});
+
+it('rest on object properties', () => {});
